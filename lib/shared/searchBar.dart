3f7899ae78b621574bf81731034d7627f3bdc0ba
mohamedmousa153 <mohamedamousa153@gmail.com>
2022-03-18 23:54:13 +0200

    list doctor and congerats

---
+++ lib/shared/searchBar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../constant.dart';
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width * 0.7,
+          height: MediaQuery.of(context).size.height * 0.06,
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: kSearchBackgroundColor,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: TextField(
+              decoration: InputDecoration.collapsed(
+                hintText: 'Search for doctors',
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: MaterialButton(
+            onPressed: () {},
+            color: Colors.blue,
+            padding: EdgeInsets.symmetric(
+              horizontal: 10,
+              vertical: 15,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: SvgPicture.asset('assets/icons/search.svg'),
+          ),
+        ),
+      ],
+    );
+  }
+}
