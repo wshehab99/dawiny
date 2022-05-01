5ef8b6a4e4329838cc21207563f693ebc0951237
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-05-01 03:24:23 +0200

    Add Native Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:find_doctor/dawiny/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const Dawiny());
+  FlutterNativeSplash.remove();
 }
