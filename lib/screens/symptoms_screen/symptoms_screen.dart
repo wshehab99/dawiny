36f0a9cafd3fb825e10ca922a1afd75235b4f575
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-05-28 19:12:23 +0200

    add ML api

--- lib/screens/symptoms_screen/symptoms_screen.dart
+++ lib/screens/symptoms_screen/symptoms_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class SymptomsScreen extends StatelessWidget {
   SymptomsScreen({Key? key}) : super(key: key);
   final TextEditingController controller = TextEditingController();
+  String response = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,164 +22,184 @@ class SymptomsScreen extends StatelessWidget {
           builder: (context, state) {
             AppCubit cubit = AppCubit.get(context);
             cubit.loadingSymptom();
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 30,
+            if (state is LoadingState) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state is ErrorgState) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Something went wrong"),
+                    ElevatedButton(
+                        onPressed: cubit.backToNormalState,
+                        child: const Text("back")),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Text(
-                    'Add Your Sympotoms',
-                    style: TextStyle(
-                        fontSize: 25,
-                        color: Color(0xff1171C8),
-                        fontWeight: FontWeight.bold),
+              );
+            } else if (state is DoneState) {
+              return AlertDialog(
+                clipBehavior: Clip.hardEdge,
+                title: const Text('Diagnos'),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "You may have a/an $response!",
+                      style: const TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                        "this is not alternative of doctor visit plese try to visit a doctor")
+                  ],
+                ),
+                actions: [
+                  ElevatedButton(
+                      onPressed: cubit.backToNormalState,
+                      child: const Text("back"))
+                ],
+              );
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: Text(
-                    'Add as many symptoms as you can for the most accurate results',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.blueGrey,
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Add Your Sympotoms',
+                      style: TextStyle(
+                          fontSize: 25,
+                          color: Color(0xff1171C8),
+                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                SearchTextFeild(
-                  searchTextFeildColor: Colors.grey[300],
-                  controller: controller,
-                  hint: "Search, e.g. headache",
-                  onSearch: (value) {
-                    cubit.searchOnSymptoms(value: value);
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                cubit.veiwedSymptoms.isEmpty
-                    ? Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Wrap(
-                            alignment: WrapAlignment.start,
-                            children: List.generate(
-                                cubit.selectedSymptoms.length, (index) {
-                              return Container(
-                                margin: EdgeInsets.all(2.0),
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 10.0, vertical: 4.0),
-                                decoration: BoxDecoration(
-                                    color: Color(0xFF1171C8),
-                                    borderRadius: BorderRadius.circular(16.0)),
-                                child: Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      cubit.selectedSymptoms[index],
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Container(
-                                      decoration: BoxDecoration(
-                                          color: Color(0xff0D5496),
-                                          borderRadius:
-                                              BorderRadius.circular(16.0)),
-                                      child: InkWell(
-                                          onTap: () {
-                                            cubit.deleteSymptom(index);
-                                          },
-                                          child: Icon(
-                                            Icons.close,
-                                            color: Colors.white,
-                                          )),
-                                    )
-                                  ],
-                                ),
-                              );
-                            }),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      'Add as many symptoms as you can for the most accurate results',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  SearchTextFeild(
+                    searchTextFeildColor: Colors.grey[300],
+                    controller: controller,
+                    hint: "Search, e.g. headache",
+                    onSearch: (value) {
+                      cubit.searchOnSymptoms(value: value);
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  cubit.veiwedSymptoms.isEmpty
+                      ? Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Wrap(
+                              alignment: WrapAlignment.start,
+                              children: List.generate(
+                                  cubit.selectedSymptoms.length, (index) {
+                                return Container(
+                                  margin: const EdgeInsets.all(2.0),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10.0, vertical: 4.0),
+                                  decoration: BoxDecoration(
+                                      color: const Color(0xFF1171C8),
+                                      borderRadius:
+                                          BorderRadius.circular(16.0)),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        cubit.selectedSymptoms[index],
+                                        style: const TextStyle(
+                                            color: Colors.white),
+                                      ),
+                                      const SizedBox(
+                                        width: 5,
+                                      ),
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: const Color(0xff0D5496),
+                                            borderRadius:
+                                                BorderRadius.circular(16.0)),
+                                        child: InkWell(
+                                            onTap: () {
+                                              cubit.deleteSymptom(index);
+                                            },
+                                            child: const Icon(
+                                              Icons.close,
+                                              color: Colors.white,
+                                            )),
+                                      )
+                                    ],
+                                  ),
+                                );
+                              }),
+                            ),
                           ),
-                        ),
-                        // child: GridView.builder(
-                        //   keyboardDismissBehavior:
-                        //       ScrollViewKeyboardDismissBehavior.onDrag,
-                        //   itemCount: cubit.selectedSymptoms.length,
-                        //   itemBuilder: (context, index) {
-                        //     return InkWell(
-                        //       onTap: () {
-                        //         cubit.deleteSymptom(index);
-                        //       },
-                        //       child: Center(
-                        //         child: Container(
-                        //           padding: const EdgeInsets.symmetric(
-                        //               horizontal: 20, vertical: 10),
-                        //           decoration: BoxDecoration(
-                        //               color: Colors.blue,
-                        //               borderRadius: BorderRadius.circular(25)),
-                        //           child: Text(cubit.selectedSymptoms[index]),
-                        //         ),
-                        //       ),
-                        //     );
-                        //   },
-                        //   gridDelegate:
-                        //       const SliverGridDelegateWithMaxCrossAxisExtent(
-                        //           mainAxisExtent: 60,
-                        //           maxCrossAxisExtent: 135,
-                        //           crossAxisSpacing: 0,
-                        //           //  crossAxisSpacing: 5,
-                        //           mainAxisSpacing: 5,
-                        //           childAspectRatio: 0.40),
-                        // ),
-                      )
-                    : Expanded(
-                        flex: 4,
-                        child: ListView.builder(
-                          keyboardDismissBehavior:
-                              ScrollViewKeyboardDismissBehavior.onDrag,
-                          itemCount: cubit.veiwedSymptoms.length,
-                          itemBuilder: (context, index) {
-                            return InkWell(
-                              onTap: () {
-                                cubit.selectSymptoms(index);
-                                controller.clear();
-                                cubit.searchOnSymptoms(value: controller.text);
-                              },
-                              child: Container(
-                                padding: const EdgeInsets.all(15),
-                                decoration: const BoxDecoration(
-                                    color: Color.fromARGB(255, 226, 226, 226)),
-                                child: Text(
-                                  cubit.veiwedSymptoms[index],
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
+                        )
+                      : Expanded(
+                          flex: 4,
+                          child: ListView.builder(
+                            keyboardDismissBehavior:
+                                ScrollViewKeyboardDismissBehavior.onDrag,
+                            itemCount: cubit.veiwedSymptoms.length,
+                            itemBuilder: (context, index) {
+                              return InkWell(
+                                onTap: () {
+                                  cubit.selectSymptoms(index);
+                                  controller.clear();
+                                  cubit.searchOnSymptoms(
+                                      value: controller.text);
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(15),
+                                  decoration: const BoxDecoration(
+                                      color:
+                                          Color.fromARGB(255, 226, 226, 226)),
+                                  child: Text(
+                                    cubit.veiwedSymptoms[index],
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w400,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          },
+                              );
+                            },
+                          ),
                         ),
-                      ),
-                Center(
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                          backgroundColor:
-                              MaterialStateProperty.all(Color(0xFF1171C8))),
-                      onPressed:
-                          cubit.selectedSymptoms.length >= 3 ? () {} : null,
-                      child: const Text("Continue")),
-                ),
-              ],
-            );
+                  Center(
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                const Color(0xFF1171C8))),
+                        onPressed: cubit.selectedSymptoms.length >= 3
+                            ? () async {
+                                response = await cubit.medicalDiagnosis();
+                              }
+                            : null,
+                        child: const Text("Continue")),
+                  ),
+                ],
+              );
+            }
           },
         ),
       ),
