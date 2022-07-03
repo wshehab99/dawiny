074290daff501efd43a57a5bddce346fb87ec1a3
Waleed Bin-Shehab <waleedbinshihab@gmail.com>
2022-07-03 20:45:01 +0200

    graduation day

--- lib/screens/nurse_details/nurse_details.dart
+++ lib/screens/nurse_details/nurse_details.dart
@@ -22,134 +22,142 @@ class NurseDetails extends StatelessWidget {
               builder: (context, states) {
                 AppCubit cubit = AppCubit.get(context);
                 cubit.getNurseById(id: id!);
-                return SafeArea(
-                  child: SizedBox(
-                    height: 600,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            child: Center(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(30),
-                                child: Image.network(
-                                  cubit.nurse != null
-                                      ? cubit.nurse!['imageUrl']
-                                      : "https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png",
-                                  width: 300,
-                                  height: 300,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          Center(
-                            child: Text(
-                              cubit.nurse != null
-                                  ? "${cubit.nurse!['firstName']} ${cubit.nurse!['lastName']}"
-                                  : " ",
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 5,
-                          ),
-                          Center(
-                            child: Column(
+
+                return (states is LoadingState)
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : SafeArea(
+                        child: SizedBox(
+                          height: 600,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                RatingBar.builder(
-                                  initialRating: 3,
-                                  minRating: 4.5,
-                                  direction: Axis.horizontal,
-                                  allowHalfRating: true,
-                                  itemCount: 5,
-                                  itemPadding: const EdgeInsets.symmetric(
-                                      horizontal: 4.0),
-                                  itemBuilder: (context, _) => const Icon(
-                                    Icons.star,
-                                    size: 10,
-                                    color: Colors.amber,
-                                  ),
-                                  onRatingUpdate: (rating) {},
-                                ),
-                                const SizedBox(
-                                  height: 10,
-                                ),
-                                const Text(
-                                  "4.8 (by 1500 Reviews)",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10),
-                            child: Row(
-                              children: [
-                                ElevatedButton(
-                                  child: SvgPicture.asset(
-                                    "assets/icons/call.svg",
-                                    height: 20,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () async {
-                                    await FlutterPhoneDirectCaller.callNumber(
-                                        number);
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
+                                Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10)),
+                                  child: Center(
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(30),
+                                      child: cubit.nurse != null
+                                          ? Image.network(
+                                              cubit.nurse!['imageUrl'] ??
+                                                  "https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png",
+                                              width: 300,
+                                              height: 300,
+                                              fit: BoxFit.cover,
+                                            )
+                                          : const SizedBox(),
                                     ),
                                   ),
                                 ),
                                 const SizedBox(
-                                  width: 20,
+                                  height: 15,
                                 ),
-                                Text(
-                                  number,
-                                  style: const TextStyle(
-                                    fontSize: 18,
+                                Center(
+                                  child: Text(
+                                    cubit.nurse != null
+                                        ? "${cubit.nurse!['firstName']} ${cubit.nurse!['lastName']}"
+                                        : " ",
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold),
                                   ),
                                 ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 15),
-                            child: Text(
-                              "Drscription Of Nurse:",
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 15),
-                            child: Text(
-                              "Nurses plan and provide medical and nursing care to patients in hospitals, at home, or in other settings who are suffering from chronic or acute physical or mental ill-health.",
-                              style: TextStyle(
-                                  fontSize: 16, color: Colors.grey[600]),
-                            ),
-                          ),
-                        ]),
-                  ),
-                );
+                                const SizedBox(
+                                  height: 5,
+                                ),
+                                Center(
+                                  child: Column(
+                                    children: [
+                                      RatingBar.builder(
+                                        initialRating: 3,
+                                        minRating: 4.5,
+                                        direction: Axis.horizontal,
+                                        allowHalfRating: true,
+                                        itemCount: 5,
+                                        itemPadding: const EdgeInsets.symmetric(
+                                            horizontal: 4.0),
+                                        itemBuilder: (context, _) => const Icon(
+                                          Icons.star,
+                                          size: 10,
+                                          color: Colors.amber,
+                                        ),
+                                        onRatingUpdate: (rating) {},
+                                      ),
+                                      const SizedBox(
+                                        height: 10,
+                                      ),
+                                      const Text(
+                                        "4.8 (by 1500 Reviews)",
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10),
+                                  child: Row(
+                                    children: [
+                                      ElevatedButton(
+                                        child: SvgPicture.asset(
+                                          "assets/icons/call.svg",
+                                          height: 20,
+                                          color: Colors.white,
+                                        ),
+                                        onPressed: () async {
+                                          await FlutterPhoneDirectCaller
+                                              .callNumber(number);
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 20,
+                                      ),
+                                      Text(
+                                        number,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(left: 15),
+                                  child: Text(
+                                    "Drscription Of Nurse:",
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 15),
+                                  child: Text(
+                                    "Nurses plan and provide medical and nursing care to patients in hospitals, at home, or in other settings who are suffering from chronic or acute physical or mental ill-health.",
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.grey[600]),
+                                  ),
+                                ),
+                              ]),
+                        ),
+                      );
               },
             ),
           )),
